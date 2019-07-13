ad02116bf33cd6f27716b59937c993c1f6670f69
Trey Arnold <arnold.trey80@gmail.com>
2019-07-12 21:10:57 -0500

    initial file structure

---
+++ src/components/Game/Game.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Game = () => (
+  <h2>Game</h2>
+)
+
+export default Game;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => (
+  <h2>Navbar</h2>
+)
+
+export default Navbar;
